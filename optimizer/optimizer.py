5e922a1d863f34318c892329ce85805a83d1ef23
A-A7med-i <an223602@gmail.com>
2025-08-22 21:24:26 +0300

    Optimizer class

---
+++ optimizer/optimizer.py
@@ -0,0 +1,29 @@
+from layer.layers import Dense
+from .base import Optimizer, Layer
+
+
+class GD(Optimizer):
+    """Optimizer that implements the Gradient Descent algorithm.
+
+    This optimizer updates the weights and biases of a layer by taking
+    a step in the opposite direction of the gradient, scaled by the
+    learning rate.
+    """
+
+    def __init__(self, learning_rate = 0.01):
+        super().__init__(learning_rate)
+
+    def update_params(self, layer: Layer):
+        """Updates the weights and biases of a dense layer using Gradient Descent.
+
+        This method checks if the layer is an instance of `Dense` and, if so,
+        applies the update rule: `param = param - learning_rate * grad`.
+
+        Args:
+            layer: An instance of a `Layer` whose parameters (weights and biases)
+                    are to be updated. It is expected to be a `Dense` layer
+                    for this optimizer to function.
+        """
+        if isinstance(layer, Dense):
+            layer.weights -= self.learning_rate * layer.weight_grad
+            layer.bias -= self.learning_rate * layer.bias_grad
